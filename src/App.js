aedeffdc67434c86aac6e9bf378adaf90e330cdf
MatheusAnadi <matheusararuna@anadi.com.br>
2019-12-03 10:12:47 -0300

    Atualizando arquivos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   const drag = useRef(null);
+  const dd = useRef(null);
+  const items = document.getElementsByClassName('item');
   useEffect(() => {
     click();
   });
@@ -15,8 +17,11 @@ function App() {
     let pos2 = 0;
     let posX = 0;
     const draga = drag.current;
-    const divWidth = drag.current.offsetWidth/3;
-
+    const divWidth = dd.current.offsetWidth;
+    const teste = dd.current;
+    const tam = items.length;
+    let maxDivWidth = divWidth*tam
+    console.log(maxDivWidth);
     //Eventos
     draga.addEventListener('touchstart', dragStart);
     draga.addEventListener('touchend', dragEnd);
@@ -54,30 +59,30 @@ function App() {
             draga.style.left = 0+'px'
             posX = 0;
           }else{
-            posX >= 600 ? posX=600 :draga.style.left = posX+'px';
+            posX >= (divWidth*2) ? posX=(divWidth*2) :draga.style.left = posX+'px';
           }
         console.log('1--- '+posX)
         }else if(posEnd<(posIni-50)){
           posX = posX - divWidth;     
-          posX <= -600 ? posX = -600 : console.log('n e menor n krai');
-          draga.offsetLeft <= -600 ? draga.style.left = -600+'px' : draga.style.left = posX+'px';          
+          posX <= -(divWidth*2) ? posX = -(divWidth*2) : console.log('n e menor n krai');
+          draga.offsetLeft <= -(divWidth*2) ? draga.style.left = -(divWidth*2)+'px' : draga.style.left = posX+'px';          
+          draga.style.WebkitTransition = 'all 1s';
         }
     }
   }
 
 
-  const Array = ['Log 1','Log 2','Log 3'];
+  const Array = ['Log 1','Log 2','Log 3','Log 4','Log 5'];
 
   return (
     <div className="App">
       <div className="wrap">
         <div className="obj" ref={drag}>
           {Array.map(item => {
-            return <div className="item">{item}</div> 
+            return <div ref={dd} className="item">{item}</div> 
           })}
         </div>
       </div>
-      <button id="tll">testando</button>
     </div>
   );
 }
